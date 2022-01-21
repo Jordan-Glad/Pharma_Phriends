9f7f8dd4bf4276602c75f2d6e7e68b51711b3cfa
Jordan Glad <jordanglad8@gmail.com>
2022-01-20 19:02:04 -0600

    Added controllers and views with basic formatting

---
+++ Pharma_Phriends/Pharma_Phriends/Controllers/SearchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pharma_Phriends.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Pharma_Phriends/Pharma_Phriends/Models/CVS.cs
@@ -0,0 +1,9 @@
+﻿namespace Pharma_Phriends.Models
+{
+    public class CVS
+    {
+        public int CvsId { get; set; }
+        public string OpenHours { get; set; }
+        public int ZipCode { get; set; }
+    }
+}
