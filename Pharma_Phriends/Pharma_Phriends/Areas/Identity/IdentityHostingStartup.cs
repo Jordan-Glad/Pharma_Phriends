7d34646a5916a7270b77a0443e43d04c302400c0
Jordan Glad <jordanglad8@gmail.com>
2022-01-20 18:34:53 -0600

    Created gitignore+Base project with auth

---
+++ Pharma_Phriends/Pharma_Phriends/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Pharma_Phriends.Data;
+
+[assembly: HostingStartup(typeof(Pharma_Phriends.Areas.Identity.IdentityHostingStartup))]
+namespace Pharma_Phriends.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
