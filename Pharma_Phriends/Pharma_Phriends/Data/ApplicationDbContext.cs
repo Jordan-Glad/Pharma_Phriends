7aadb8706c5d884f3011d5c2375de214127105ed
Jordan Glad <jordanglad8@gmail.com>
2022-01-20 18:43:59 -0600

    added application user and dbset for users

--- Pharma_Phriends/Pharma_Phriends/Data/ApplicationDbContext.cs
+++ Pharma_Phriends/Pharma_Phriends/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Pharma_Phriends.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +9,7 @@ namespace Pharma_Phriends.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<ApplicationUser> users { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
